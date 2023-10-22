b4079d3f02c5bd7e5701d3f2237183c3086a9e47
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-22 17:46:17 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Declare variables for user input and total
+string input;
+int total = 0;
+
+// Print Title
+Console.WriteLine("Running Total");
+Console.WriteLine("=============");
+
+// Loop until Q is enetered
+do
+{
+    // Prompt user
+    Console.Write("Enter a number (Q to quit): ");
+    input = Console.ReadLine().ToUpper();
+
+    // if Q break loop
+    if (input != "Q")
+    {
+        // parse and add to total
+        total += int.Parse(input);
+    }
+} while (input != "Q");
+
+// Print result
+Console.WriteLine($"The sum of the number is {total}.");
